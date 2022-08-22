2f7161a0acedfd08447c2625f862c359f94b0a6f
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-22 10:45:55 +0600

    player add dynamically done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-// ==========> common function <==========
-// const stringConverter = (string) =>{
-//    const number = parseFloat(string.value);
-// }
+const topFivePlayersWrapper = document.getElementById('top_5_list');
 
+// ==========> player added dynamically <==========
+const buttons = document.getElementsByClassName('select_button');
+for (const button of buttons) {
+    button.addEventListener('click', (e) => {
+        // console.log(e.target.parentNode.children[0].innerText)
+        topFivePlayersWrapper.innerHTML += `
+        <li>${e.target.parentNode.children[0].innerText}</li>
+        `;
+    })
+
+}
