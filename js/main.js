a9ef2c8ea50aee9a1ab0a3cacc3feacacee70962
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-22 13:30:25 +0600

    calculate expenses total done

--- js/main.js
+++ js/main.js
@@ -1,23 +1,61 @@
 const topFivePlayersWrapper = document.getElementById('top_5_list');
-const li = document.getElementsByClassName('li');
-// console.log(topFivePlayersWrapper.children)
-const playerLength = topFivePlayersWrapper?.children.length - 1;
+const player_expenses_value = document.getElementById('player_expenses_value');
+// const perPlayerInput = document.getElementById('per_player_input');
 
+// ==========> common function <==========
+// ----->get input id & return value
+function getInputId(idName) {
+    const inputId = document.getElementById(idName);
+    const finalOutput = stringToNumber(inputId.value);
+    return finalOutput;
+}
+function stringToNumber(string) {
+    const number = parseFloat(string);
+    return number;
+}
+//-----> calculate
+function calculate(pLength) {
+    // ---> player length
+    let playerLength = pLength;
+    return playerLength;
+}
 
 // ==========> player added dynamically <==========
+
 const buttons = document.getElementsByClassName('select_button');
 for (const button of buttons) {
     // -----> children count <-----
     button.addEventListener('click', function (e) {
-        const playerLength = topFivePlayersWrapper?.children.length - 1;
-        if (playerLength < 4) {
+        const playerLength = topFivePlayersWrapper?.children.length;
+        if (playerLength < 5) {
             topFivePlayersWrapper.innerHTML += `
             <li>${e.target.parentNode.children[0].innerText}</li>
             `;
+            let players = calculate(topFivePlayersWrapper.children.length);
+            //-----> calculate total
+            document.getElementById('calculate_btn').addEventListener('click', function (e) {
+                e.stopPropagation();
+                const perPlayer = getInputId('per_player_input');
+                const playerExpenses = players * perPlayer;
+                player_expenses_value.innerText = playerExpenses;
+                console.log(playerExpenses)
+            })
+
         } else {
             alert('You can select 5 players at a time')
         }
-
     })
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
