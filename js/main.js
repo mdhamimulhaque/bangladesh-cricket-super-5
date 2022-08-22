0a92a5173db11fe5421b078df6efb487da26054f
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-22 10:47:43 +0600

    top five added done

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const topFivePlayersWrapper = document.getElementById('top_5_list');
 const buttons = document.getElementsByClassName('select_button');
 for (const button of buttons) {
     button.addEventListener('click', (e) => {
-        // console.log(e.target.parentNode.children[0].innerText)
         topFivePlayersWrapper.innerHTML += `
         <li>${e.target.parentNode.children[0].innerText}</li>
         `;
