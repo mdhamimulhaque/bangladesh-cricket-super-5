0fb762b14775b9e79cd878bfdab54141c863e3ab
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-22 11:40:31 +0600

    alert add if added over 5 player

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 const topFivePlayersWrapper = document.getElementById('top_5_list');
+const li = document.getElementsByClassName('li');
+// console.log(topFivePlayersWrapper.children)
+const playerLength = topFivePlayersWrapper?.children.length - 1;
+
 
 // ==========> player added dynamically <==========
 const buttons = document.getElementsByClassName('select_button');
 for (const button of buttons) {
-    button.addEventListener('click', (e) => {
-        topFivePlayersWrapper.innerHTML += `
-        <li>${e.target.parentNode.children[0].innerText}</li>
-        `;
+    // -----> children count <-----
+    button.addEventListener('click', function (e) {
+        const playerLength = topFivePlayersWrapper?.children.length - 1;
+        if (playerLength < 4) {
+            topFivePlayersWrapper.innerHTML += `
+            <li>${e.target.parentNode.children[0].innerText}</li>
+            `;
+        } else {
+            alert('You can select 5 players at a time')
+        }
+
     })
 
 }
